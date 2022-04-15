83074fcebbd443a8d61c0dc4a0a151dbd11cd4e9
boss <andryousha228@gmail.com>
2022-04-15 22:13:47 +0500

    routes is works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header'
@@ -7,22 +7,25 @@ import Nav from './components/Nav/Nav'
 import Profile from './components/Profile/Profile'
 
 
+
 const app = () => {
     return (
-        <BrowserRouter>
+
         <div className='app-wrapper'>
             <Header />
             <Nav />
-            
+
             <div className='app-wrapper-content'>
-                <Route path='/dialogs' component={Dialogs}/>
-                <Route path='/profile' component={Profile}/> 
-            
-            
-            </div> 
+                <Routes>
+                    <Route path='/dialogs' element={<Dialogs />} />
+                    <Route path='/profile' element={<Profile />} />
+                </Routes>
+
+
+            </div>
 
         </div>
-        </BrowserRouter>
+
     )
 }
 
