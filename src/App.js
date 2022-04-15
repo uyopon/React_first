035dc7bbb98b02749b6dbf42cdfa7c3d4f51b46c
boss <andryousha228@gmail.com>
2022-04-15 19:45:20 +0500

    where the bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header'
@@ -8,15 +9,20 @@ import Profile from './components/Profile/Profile'
 
 const app = () => {
     return (
+        <BrowserRouter>
         <div className='app-wrapper'>
             <Header />
             <Nav />
-            {/* <Profile/>  */}
+            
             <div className='app-wrapper-content'>
-                <Dialogs />
+                <Route path='/dialogs' component={Dialogs}/>
+                <Route path='/profile' component={Profile}/> 
+            
+            
             </div> 
 
         </div>
+        </BrowserRouter>
     )
 }
 
