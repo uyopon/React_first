2b01061c278338ce69cd679d7cfdcc54fdcc90b2
boss <andryousha228@gmail.com>
2022-04-20 01:06:33 +0500

    not displayed

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css'
 
+
+
+//-----------------------------КОМПОНЕНТА МЕНЯЮЩАЯ URL----------------------------------------
 const DialogItem = (props) => {
     let path = "/Dialogs/" + props.id;
     return (
@@ -13,6 +16,12 @@ const DialogItem = (props) => {
     )
 }
 
+
+
+
+
+
+
 const Message = (props) => {
     return (
         <div className={s.dialog}>
@@ -21,24 +30,66 @@ const Message = (props) => {
     )
 }
 
+
+
+
+//--------------------DATA FOR ARRAYS THAT 
+
 const Dialogs = (props) => {
+    let DialogsData = [
+        { id: 1, name: 'Dima' },
+        { id: 2, name: 'Andrey' },
+        { id: 3, name: 'Sveta' },
+        { id: 4, name: 'Sasha' },
+        { id: 5, name: 'Viktor' },
+        { id: 6, name: 'Valera' },
+    ]
+    let MessagesData = [
+        { id: 1, message: 'hi' },
+        { id: 2, message: 'how are you' },
+        { id: 3, message: 'yo' },
+        { id: 4, message: 'hi' },
+        { id: 5, message: 'hi Viktor' },]
+
+
+
+
+//CREATING OF ARRAYS THAT
+
+    let dialogsElement = DialogsData.map(d => { <DialogItem name={d.name} id={d.id} /> })
+{/* <DialogItem name={DialogsData[0].name} id={DialogsData[0].id} />
+    <DialogItem name={DialogsData[1].name} id={DialogsData[1].id} />
+    <DialogItem name={DialogsData[2].name} id={DialogsData[2].id} />
+    <DialogItem name={DialogsData[3].name} id={DialogsData[3].id} />
+    <DialogItem name={DialogsData[4].name} id={DialogsData[4].id} />
+    <DialogItem name={DialogsData[5].name} id={DialogsData[5].id} /> */}
+
+    let messagesElement = MessagesData.map(m => { <Message message={m.message} id={m.id} /> })
+{/* <Message message={MessagesData[0].message} id={MessagesData[0].id} />
+    <Message message={MessagesData[1].message} id={MessagesData[1].id} />
+    <Message message={MessagesData[2].message} id={MessagesData[2].id} />
+    <Message message={MessagesData[3].message} id={MessagesData[3].id} />
+    <Message message={MessagesData[4].message} id={MessagesData[4].id} /> */}
+   
+
+
+
+
+    
+
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogs_items}>
-                <DialogItem name="Dima" id="1" />
-                <DialogItem name="Andrey" id="2" />
-                <DialogItem name="Sveta" id="3" />
-                <DialogItem name="Sasha" id="4" />
-                <DialogItem name="Viktor" id="5" />
-                <DialogItem name="Valera" id="6" />
+
+            {dialogsElement}
+
             </div>
             <div className={s.messages}>
-                <Message message='hi' />
-                <Message message='how are you' />
-                <Message message='yo' />
-                <Message message='hi' />
-                <Message message='hi' />
-                <Message message='hi' />
+
+                { messagesElement}
+
+
             </div>
         </div>
     )
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,10 +3,18 @@ import s from './MyPosts.module.css'
 import Post from './Post/Post'
 
 const MyPosts = () => {
+
+    let PostData =[
+        {id:1,message:'Hi guys',LikesCount:2},
+        {id:2,message:'hello everybody',LikesCount:0},
+        {id:3,message:'my name is john',LikesCount:1},
+        {id:4,message:'I am  here',LikesCount:8},
+    ]
+
+
     return (
 
         <div className={s.padding}>
-
 
             <div className={s.border}>
                 my posts
@@ -16,10 +24,10 @@ const MyPosts = () => {
             </div>
 
             <div >
-                <Post message='Hi guys' LikesCount = '2'/>
-                <Post message='hello everybody' LikesCount = '0'/>
-                <Post message='my name is john' LikesCount = '1'/>
-                <Post message='I am  here' LikesCount = '8'/>
+                <Post message={PostData[0].message} id={PostData[0].id} LikesCount = {PostData[0].LikesCount}/>
+                <Post message={PostData[1].message} id={PostData[1].id} LikesCount = {PostData[1].LikesCount}/>
+                <Post message ={PostData[2].message} id={PostData[2].id} LikesCount = {PostData[2].LikesCount}/>
+                <Post message = {PostData[3].message} id={PostData[3].id} LikesCount = {PostData[3].LikesCount}/>
             </div>
         </div>
     )
