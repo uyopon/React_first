fd82fe1c072d01597f13663c504b5dbb10ac4cf7
boss <andryousha228@gmail.com>
2022-04-22 22:53:21 +0500

    new

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import s from './MyPosts.module.css'
 import Post from './Post/Post'
 
+
+
+
 const MyPosts = (props) => {
 
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!NOT DISPLAYED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-    
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!NOT DISPLAYED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!OK NOW 
+let postsElements = props.Posts.map(p => <Post message={p.message} id={p.id} LikesCount={p.LikesCount} />  )
+
+
 
     return (
 
@@ -22,12 +25,9 @@ const MyPosts = (props) => {
             </div>
 
             <div >
+                {postsElements}
                
-                333333333333333333333333333333333
-                {/* <Post message={PostData[0].message} id={PostData[0].id} LikesCount = {PostData[0].LikesCount}/>
-                <Post message={PostData[1].message} id={PostData[1].id} LikesCount = {PostData[1].LikesCount}/>
-                <Post message ={PostData[2].message} id={PostData[2].id} LikesCount = {PostData[2].LikesCount}/>
-                <Post message = {PostData[3].message} id={PostData[3].id} LikesCount = {PostData[3].LikesCount}/> */}
+
             </div>
         </div>
     )
