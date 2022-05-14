cc6c1c7f09aa518a535c3d1b6b84cd2b1b95d7e6
boss <andryousha228@gmail.com>
2022-05-15 03:19:43 +0500

    3:20

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -4,64 +4,38 @@ import s from './Dialogs.module.css'
 import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
 
-
 const Dialogs = (props) => {
-
     let newMessageElement = React.createRef();
-
     let addMessage = () => {
         let text = newMessageElement.current.value;
         alert(text)
     }
-
-
-
-
-
     let dialogsElement = props.Dialogs.map(d => <DialogItem name={d.name} id={d.id} />)
-
-
-
-
     let messagesElement = props.Messages.map(m => <Message message={m.message} id={m.id} />)
-
-
+    let onsendmessageclick = () => { }
     return (
         <div className={s.dialogs}>
             <div className={s.dialogs_items}>
-
-
                 {dialogsElement}
-
             </div>
             <div className={s.messages}>
 
-
-
                 <div>{messagesElement}</div>
                 <div>
-                    <textarea className={s.margin_left} ref={newMessageElement}
+                    <textarea
+                        className={s.margin_left}
+                        ref={newMessageElement}
                         value='2'
-
-
                     ></textarea>
                     <div className={s.margin_left} >
-                        <button >send</button>
+                        <button
+                            onClick={onsendmessageclick}
+                        >send</button>
                     </div>
-
                 </div>
-
-                
-
-
-
-
-
             </div>
         </div>
     )
 }
-
-
 
 export default Dialogs;
