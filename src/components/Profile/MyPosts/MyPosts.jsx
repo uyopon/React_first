65cec6cf52fd18e862e86de23a19dd3946264de4
boss <andryousha228@gmail.com>
2022-05-15 20:10:53 +0500

    may 15

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { addPostActionCreator, updateNewPostTextActionCretor } from '../../../redux/state';
+import { addPostActionCreator, updateNewPostTextActionCretor } from '../../../redux/state'; //------IMPORT ACTION(CALLBACKS ВОЗВРАЩАЮЩИЕ ACTION (ОБЪЕКТЫ СО СВОЙСТВОМ TYPE))
 import s from './MyPosts.module.css'
 import Post from './Post/Post'
 
@@ -11,25 +11,25 @@ const MyPosts = (props) => {
     let newPostElement =React.createRef();
 
     let addPost=()=>{
-        let action = addPostActionCreator()
-        props.dispatch(action)}
+        let action = addPostActionCreator()//------------------ВЫЗОВ ACTION (F ВОЗВРАЩАЮЩАЯ ACTION)
+        props.dispatch(action)} //-------------------------------------ВЫЗОВ DISPATH CALLBACH ХРАНЯЩИ ЗАПРОСЫ ДЛ ИЗМЕНЕНИ STATE
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        let action = updateNewPostTextActionCretor(text)
-        props.dispatch(action)}
+        let action = updateNewPostTextActionCretor(text)//------------------ВЫЗОВ ACTION (F ВОЗВРАЩАЮЩАЯ ACTION)
+        props.dispatch(action)} //-----------------------------------ВЫЗОВ DISPATH CALLBACH ХРАНЯЩИ ЗАПРОСЫ ДЛ ИЗМЕНЕНИ STATE
 
     return (
         <div className={s.padding}>
-
-            <textarea name="Type" id="" cols="50" rows="5" 
+           
+            <textarea name="Type" id="" cols="50" rows="5" //------------------------------------------------------TEXT-AREA
             className={s.margin_left} 
             ref={newPostElement} 
             value = {props.newPostsText}
-            onChange = {onPostChange}></textarea>
+            onChange = {onPostChange}></textarea> 
 
-            <div className={s.margin_left} >
-                <button onClick={addPost}>add post</button>
+            <div className={s.margin_left} > 
+                <button onClick={addPost}>add post</button> 
             </div>
 
             <div className={s.border}>
