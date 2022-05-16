218fc2073a2ad4a32f1e31c04c48925da0e2ceb9
boss <andryousha228@gmail.com>
2022-05-17 03:08:15 +0500

    3:00

---
+++ src/redux/dialogsPage_reducer.js
@@ -0,0 +1,21 @@
+const SEND_MESSAGE = 'SEND-MESSAGE'
+const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT'     //ИСПОЛЬЗУЕМ ЭТИ КОНСТАНТЫ ЧТОБЫ НЕ ОПЕРЧАТАТЬСЯ  = )
+
+const dialogsPage_reducer = (state, action) => {
+
+    if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
+        state.dialogsPage.newMessageBody = action.body
+
+    }
+    else if (action.type === SEND_MESSAGE) {
+        let body = this._state.dialogsPage.newMessageBody
+        state.dialogsPage.newMessageBody = ''
+        state.dialogsPage.Messages.push({ id: 6, message: body })
+    }
+    return state
+}
+
+
+export default dialogsPage_reducer
+
+
