c89e99c46263c52d8d22f99b42ec313ab1386b79
boss <andryousha228@gmail.com>
2022-06-12 22:01:31 +0500

    npm install react-redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import reportWebVitals from './reportWebVitals';
-import store from './redux/store';
+import store from './redux/redux-store';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
