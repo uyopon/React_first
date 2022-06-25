c58d8c788e40fa1bf2e4569aa39163a66df95eca
boss <andryousha228@gmail.com>
2022-06-25 22:43:22 +0500

    descriptions

--- src/components/Users/UsersC.jsx
+++ src/components/Users/UsersC.jsx
@@ -4,13 +4,20 @@ import * as axios from 'axios'
 import ava from '../../assets/img/ava.png'
 
 class Users extends React.Component {
+    // Users: [],
+    // pageSize:100,
+    // totalUsersCount: 0,
+    // currentPage: 1,
+
     componentDidMount(){
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
             //https://social-network.samuraijs.com/api/1.0/users - вбиваем в браузер и смотрит get request  
-            this.props.setUsersAC(response.data.items)
-            this.props.setTotalUsersCountAC(response.data.totalCount)
+            this.props.setUsersAC(response.data.items)//пользователи
+            this.props.setTotalUsersCountAC(response.data.totalCount)//кол-во всех пользователей
+            debugger
+
         })
-    }
+    }  
     onPageChanged=(pageNumber)=>{ 
         this.props.setCurrentPage(pageNumber)
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
@@ -18,14 +25,14 @@ class Users extends React.Component {
             this.props.setUsersAC(response.data.items)
         })
     }
-
     render() {
-        let pagesCount = Math.ceil(this.props.totalUsersCount/ this.props.pageSize) 
+        let pagesCount = Math.ceil(this.props.totalUsersCount/ this.props.pageSize)//this.props.pageSize =100
+
         let pages = []
         for(let i=1; i<=pagesCount;i++ ){ //
             pages.push(i)
         }
-        console.log(this.props)
+        
         return (
             <div className={s.container} >
                 <div>
